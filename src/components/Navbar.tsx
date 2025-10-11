96e906707cf6604048ab4095a85246a221119fe3
Nishant <onenish14@gmail.com>
2025-10-11 20:39:25 +0530

    start

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+         {/*Logo */}
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold font-mono tracking-wider"
+            >
+              🌱 Plantventory
+            </Link>
+          </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
