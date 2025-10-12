9eacc6ed5ab693ae8ff63683f1ea1ced7b00b21c
Nishant <onenish14@gmail.com>
2025-10-12 20:49:22 +0530

    ui

--- src/app/plants/[slug]/PlantCard.tsx
+++ src/app/plants/[slug]/PlantCard.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-const PlantCard = () => {
+import { Badge } from "../../../components/ui/badge";
+
+export default function PlantCard() {
+
+    
+
   return (
-    <div>PlantCard slug</div>
-  )
+    <Card className="max-w">
+      <div className="flex flex-row">
+        <div className="basis-2/4">
+          <CardHeader>
+          </CardHeader>
+        </div>
+        <div className="basis-2/4 flex flex-col justify-between">
+          <CardContent className="mt-8 space-y-3">
+            <CardTitle className="text-5xl font-bold">aleo wera</CardTitle>
+            <CardTitle className="text-3xl font-bold">${20}</CardTitle>
+            <Badge>aise hi</Badge>
+            <CardDescription>Stock: 20</CardDescription>
+            <CardDescription className="text-white">
+              description
+            </CardDescription>
+          </CardContent>
+        </div>
+      </div>
+    </Card>
+  );
 }
-
-export default PlantCard
