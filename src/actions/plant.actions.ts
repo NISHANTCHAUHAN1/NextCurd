642c977ae90d9be26ce8410eca6c6bf300182bc5
Nishant <onenish14@gmail.com>
2025-10-12 19:30:18 +0530

    getPlants

--- src/actions/plant.actions.ts
+++ src/actions/plant.actions.ts
@@ -1,7 +1,28 @@
+import { prisma } from "@/lib/prisma";
+import { getUserId } from "./user.actions";
+import { revalidatePath } from "next/cache";
+
 export async function getPlants(serachTerm?: String) {
-    try {
-        
-    } catch (error) {
-        console.log("Error in getPlants", error);
+  try {
+    const currentUserID = await getUserId();
+    const whereClause: any = {
+      userId: currentUserID,
+    };
+
+    if (serachTerm) {
+      whereClause.name = {
+        contains: serachTerm,
+        mode: "insensitive",
+      };
     }
+
+    const userPlants = await prisma.plants.findMany({
+      where: whereClause,
+    });
+
+    revalidatePath("/");
+    return { success: true, userPlants };
+  } catch (error) {
+    console.log("Error in getPlants", error);
+  }
 }
